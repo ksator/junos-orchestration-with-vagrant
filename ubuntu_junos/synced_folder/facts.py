5f79d5d4d9b55187963fd0d5c55eb9f0e8e58fef
ksator2 <ksator@juniper.net>
2016-05-30 16:37:57 +0200

    Update facts.py

--- ubuntu_junos/synced_folder/facts.py
+++ ubuntu_junos/synced_folder/facts.py
@@ -1,5 +1,5 @@
 from jnpr.junos import Device
-dev=Device(host="10.0.1.10", user="khelil", password="Juniper123")
+dev=Device(host="10.0.1.10", user="root", password="Juniper123")
 dev.open()
 print ("the device "+ dev.facts["hostname"]+ " is a " + dev.facts['model'] + " running " + dev.facts["version"])
 dev.close()
